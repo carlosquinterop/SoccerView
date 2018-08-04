bc4bef66191252fbac1245ebc93f99c9e814590b
carlosquinterop <quintero.carlos@uniandes.edu.co>
2018-08-04 15:52:05 -0500

    Removed kdevelop file

---
+++ build/CMakeFiles/soccerviewer.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jess-linux/Documents/Projects/SoccerView/src/DigitalClock.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/DigitalClock.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/src/SoccerViewer.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/SoccerViewer.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/src/cameraThread.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/cameraThread.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/src/main.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/main.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/build/src/moc_DigitalClock.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/moc_DigitalClock.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/build/src/moc_SoccerViewer.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/moc_SoccerViewer.cpp.o"
+  "/home/jess-linux/Documents/Projects/SoccerView/build/src/moc_cameraThread.cpp" "/home/jess-linux/Documents/Projects/SoccerView/build/CMakeFiles/soccerviewer.dir/src/moc_cameraThread.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
